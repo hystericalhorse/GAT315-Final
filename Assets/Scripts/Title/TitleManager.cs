e3b5bd32f88f0de356016ac7d57d2574c70ee7c2
ZachLovato <zlovato@student.neumont.edu>
2023-05-27 13:45:20 -0600

    Title Scene
    Setup a template title screen with title manager

---
+++ Assets/Scripts/Title/TitleManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField] string sceneName;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void StartGame()
+	{
+        // changes scene basied on scene name
+        SceneManager.LoadScene(sceneName);
+	}
+
+	public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+
+
+   
+}
