8c788ab34c6b20115f0b0b170a1bc0e8b53ad551
ZachLovato <zlovato@student.neumont.edu>
2023-05-31 14:47:48 -0600

    created small rooms to show players buttons and gravity lift

--- Assets/Scripts/PlayerSpawn.cs
+++ Assets/Scripts/PlayerSpawn.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class PlayerSpawn : MonoBehaviour
 {
     [SerializeField] GameObject Player;
+	int alive = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         Player.transform.position = transform.position;
     }
+	private void LateUpdate()
+	{
+		Player.transform.position = transform.position;
+		//Destroy(this);
+	}
 
-    // Update is called once per frame
-    void Update()
+	// Update is called once per frame
+	void Update()
     {
 		Player.transform.position = transform.position;
-        Destroy(this);
+		if (alive == 10) Destroy(this);
+		else alive++;
+		//Destroy(this);
 	}
 }
