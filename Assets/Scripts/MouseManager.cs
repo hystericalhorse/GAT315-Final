4c856189e6f1fcac995eb7d94ca51fe5f54bc196
Birdy Bird <edixon@student.neumont.edu>
2023-06-01 16:42:09 -0600

    Added MouseManager and Fixed Camera

---
+++ Assets/Scripts/MouseManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseManager : MonoBehaviour
+{
+	bool cursorActive = false;
+
+	private void Start()
+	{
+		
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			cursorActive = !cursorActive;
+
+			Cursor.lockState = (cursorActive) ? CursorLockMode.None : CursorLockMode.Locked;
+		}
+	}
+
+	private void LateUpdate()
+	{
+        
+    }
+}
